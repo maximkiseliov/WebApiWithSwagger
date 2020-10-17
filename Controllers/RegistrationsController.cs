c25d4594385c9ba4fa60084c7ef5e4d279c9707e
Maxim Kiseliov <maxim.kiseliov@gmail.com>
2020-10-18 00:52:31 +0300

    Base objects replaced with project related. Address model added

---
+++ Controllers/RegistrationsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TechnicalTestWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegistrationsController : ControllerBase
+    {
+        private readonly ILogger<RegistrationsController> _logger;
+
+        public RegistrationsController(ILogger<RegistrationsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "App Running...";
+        }
+
+        //[HttpGet("{id:int}")]
+        //public ActionResult<string> Get(int id)
+        //{
+        //    return _context.Addresses.Find(id).ToString();
+        //}
+
+        //[HttpPost]
+        //public ActionResult<int> Post(Address address)
+        //{
+        //    _context.Addresses.Add(address);
+        //    _context.SaveChanges();
+
+        //    return address.AddressId;
+        //}
+    }
+}
---
+++ Models/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace TechnicalTestWebApi.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string Locale { get; set; }
+        public string AddressLine1 { get; set; }
+
+        public override string ToString()
+        {
+            return $"addressLine1: {AddressLine1}, locale: {Locale}";
+        }
+    }
+}
